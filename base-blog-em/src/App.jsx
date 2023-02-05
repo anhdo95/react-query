a3956732eabef3350cbf7cf50d0db901714ac9ad
Anh Do <do.dinh.anh@linecorp.com>
2023-02-05 22:17:07 +0700

    Set up provider and fetch posts

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,13 +1,17 @@
 import { Posts } from "./Posts";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    // provide React Query client to App
+    <QueryClientProvider client={queryClient}>
     <div className="App">
       <h1>Blog Posts</h1>
       <Posts />
     </div>
+    </QueryClientProvider>
   );
 }
 
