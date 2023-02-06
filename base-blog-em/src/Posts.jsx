3a5a856abd250dd48b5d7ce010ba568b16b3ea98
Anh Do <do.dinh.anh@linecorp.com>
2023-02-06 10:17:24 +0700

    Handle pagination on posts

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -4,18 +4,18 @@ import { useQuery } from '@tanstack/react-query'
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(page) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data = [] } = useQuery(['posts'], fetchPosts, { staleTime: 10000 })
+  const { data = [] } = useQuery(['posts', currentPage], () => fetchPosts(currentPage), { staleTime: 10000 })
 
   return (
     <>
@@ -31,11 +31,11 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage <= 1} onClick={() => setCurrentPage(currentPage - 1)}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage >= maxPostPage} onClick={() => setCurrentPage(currentPage + 1)}>
           Next page
         </button>
       </div>
