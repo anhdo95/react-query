55e947bec982d4afd1a39720d249f12740e0885b
Anh Do <do.dinh.anh@linecorp.com>
2023-02-07 11:37:15 +0700

    Pre-fetching treatments

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,8 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
+import queryClient from '../../../react-query/queryClient';
 
 async function getTreatments(): Promise<Treatment[]> {
   const { data } = await axiosInstance.get('/treatments');
@@ -12,4 +14,10 @@ async function getTreatments(): Promise<Treatment[]> {
 export function useTreatments(): Treatment[] {
   const { data = [] } = useQuery([queryKeys.treatments], getTreatments);
   return data;
+}
+
+export function usePrefetchTreatments(): void {
+  useEffect(() => {
+    queryClient.prefetchQuery([queryKeys.treatments], getTreatments);
+  }, []);
 }
