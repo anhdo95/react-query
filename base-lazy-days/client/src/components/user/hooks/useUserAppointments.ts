3f5ff90f76e492fc02139eaf6ec2cf107bba7c0b
Anh Do <do.dinh.anh@linecorp.com>
2023-02-08 16:31:24 +0700

    Get user's appointments

--- base-lazy-days/client/src/components/user/hooks/useUserAppointments.ts
+++ base-lazy-days/client/src/components/user/hooks/useUserAppointments.ts
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query';
 import dayjs from 'dayjs';
 
 import type { Appointment, User } from '../../../../../shared/types';
@@ -5,18 +6,24 @@ import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useUser } from './useUser';
 
-// for when we need a query function for useQuery
-// async function getUserAppointments(
-//   user: User | null,
-// ): Promise<Appointment[] | null> {
-//   if (!user) return null;
-//   const { data } = await axiosInstance.get(`/user/${user.id}/appointments`, {
-//     headers: getJWTHeader(user),
-//   });
-//   return data.appointments;
-// }
+async function getUserAppointments(
+  user: User | null,
+): Promise<Appointment[] | null> {
+  if (!user) return null;
+  const { data } = await axiosInstance.get(`/user/${user.id}/appointments`, {
+    headers: getJWTHeader(user),
+  });
+  return data.appointments;
+}
 
 export function useUserAppointments(): Appointment[] {
-  // TODO replace with React Query
-  return [];
+  const { user } = useUser();
+
+  const { data: userAppointments = [] } = useQuery({
+    queryKey: [queryKeys.userAppointments],
+    queryFn: () => getUserAppointments(user),
+    enabled: !!user,
+  });
+
+  return userAppointments;
 }
